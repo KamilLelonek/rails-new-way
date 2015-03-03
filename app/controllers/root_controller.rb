09f5eff92df10ce6b3ca58cc0d0d04b66e5427d6
Kamil Lelonek <squixy.sln@gmail.com>
2015-03-03 22:33:49 +0100

    Added endpoint for categories

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,6 +2,7 @@ class RootController < ApplicationController
   def index
     render json: {
              ingredients: "#{root_url}ingredients",
+             categories:  "#{root_url}categories",
            }
   end
 end
