aea3f9055aa96993fbd6c8e5e57e53d7327a10e3
Kamil Lelonek <squixy.sln@gmail.com>
2015-03-03 22:37:08 +0100

    Testing root controller

---
+++ spec/controllers/root_controller_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe RootController do
+  it 'routes / to root#index' do
+    expect(get: '/').to route_to(
+                          controller: 'root',
+                          action:     'index'
+                        )
+  end
+
+  it 'renders API urls' do
+    xhr :get, :index
+    expect(response).to be_success
+    expect(response.body).to be_json_eql current_api
+  end
+
+  def current_api
+    {
+      categories:  "#{request.url}categories",
+      ingredients: "#{request.url}ingredients",
+    }.to_json
+  end
+
+  it 'provides routable paths' do
+    expect(get: '/categories') .to be_routable
+    expect(get: '/ingredients').to be_routable
+  end
+end
