d23e2a83b401297fca29ca4267d56241180e4de4
Kamil Lelonek <squixy.sln@gmail.com>
2015-03-07 20:19:02 +0100

    Namespacing ExistencePolicy

--- app/domain/common/existence_policy.rb
+++ app/domain/common/existence_policy.rb
@@ -1,7 +1,9 @@
-class ExistencePolicy
-  takes :repository
+module Common
+  class ExistencePolicy
+    takes :repository
 
-  def is_satisfied_by?(object_id)
-    repository.find_by(id: object_id).present?
+    def is_satisfied_by?(object_id)
+      repository.find_by(id: object_id).present?
+    end
   end
 end
