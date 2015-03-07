67e147dcb521b088174a37a6f62cd088ac8119ab
Kamil Lelonek <squixy.sln@gmail.com>
2015-03-07 18:38:50 +0100

    Fixing typo

--- db/migrate/20150307172047_create_deliveries.rb
+++ db/migrate/20150307172047_create_deliveries.rb
@@ -2,7 +2,7 @@ class CreateDeliveries < ActiveRecord::Migration
   def change
     create_table :deliveries, id: :uuid do |t|
       t.string  :name,        null: false
-      t.text    :descirption, null: false
+      t.text    :description, null: false
     end
   end
 end
