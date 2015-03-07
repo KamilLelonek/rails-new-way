a07beeb848d648a1d628dec020aa5a30d22113ba
Kamil Lelonek <squixy.sln@gmail.com>
2015-03-07 20:18:13 +0100

    Using correct names for associations

--- app/domain/offer_requests/deliveries/validate_delivery_option.rb
+++ app/domain/offer_requests/deliveries/validate_delivery_option.rb
@@ -1,22 +1,22 @@
 module OfferRequests
   module Deliveries
     class ValidateDeliveryOption
-      takes :delivery_option_repository
+      takes :delivery_repository
 
       InvalidDeliveryOption = Class.new(StandardError) do
         takes :kind, :errors
       end
 
-      def call(delivery_option_id)
-        raise InvalidDeliveryOption.new :invalid_attributes, errors: { delivery_option_id: 'delivery option does not exist' } unless delivery_option_exists?(delivery_option_id)
+      def call(delivery_id)
+        raise InvalidDeliveryOption.new :invalid_attributes, errors: { delivery_id: 'delivery option does not exist' } unless delivery_exists?(delivery_id)
       end
 
-      def delivery_option_exists?(delivery_option_id)
-        devliery_option(delivery_option_id).present?
+      def delivery_exists?(delivery_id)
+        devliery(delivery_id).present?
       end
 
-      def devliery_option(delivery_option_id)
-        @devliery_option ||= delivery_option_repository.find_by(id: delivery_option_id)
+      def devliery(delivery_id)
+        @devliery ||= delivery_repository.find_by(id: delivery_id)
       end
     end
   end
--- app/domain/offer_requests/products/product_entity.rb
+++ app/domain/offer_requests/products/product_entity.rb
@@ -6,12 +6,12 @@ module OfferRequests
       include ::Virtus.model
 
       attribute :id, String
-      attribute :juice_category_id, String, default: :default_juice_category_id
-      attribute :juice_category,    Juices::Categories::CategoryEntity
-      attribute :quantity,          Integer
+      attribute :category_id,    String, default: :default_category_id
+      attribute :juice_category, Juices::Categories::CategoryEntity
+      attribute :quantity,       Integer
 
       private
-      def default_juice_category_id
+      def default_category_id
         juice_category.id if juice_category.present?
       end
     end
